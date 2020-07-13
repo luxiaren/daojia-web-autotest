1391491a7ee4ea1b3bd4163cb85476c2c1985f81
luxiaren <2318115449@qq.com>
2020-07-14 00:25:04 +0800

    项目立项

---
+++ o2o-account-autotest/src/main/java/com/daojia/autotest/O2oAccountAutotestApplication.java
@@ -0,0 +1,14 @@
+package com.daojia.autotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O2oAccountAutotestApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(O2oAccountAutotestApplication.class, args);
+    }
+
+}
---
+++ o2o-account-autotest/src/test/java/com/daojia/autotest/O2oAccountAutotestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.daojia.autotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O2oAccountAutotestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello springboot");
+    }
+
+}
